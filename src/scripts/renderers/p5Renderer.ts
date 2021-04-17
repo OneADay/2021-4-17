7078dd731b16dafb588b67f32a0a3e1e3e73bef0
cjgammon <gammon@adobe.com>
2021-04-17 10:34:43 -0500

    animation

--- src/scripts/renderers/p5Renderer.ts
+++ src/scripts/renderers/p5Renderer.ts
@@ -44,36 +44,44 @@ export default class P5Renderer implements BaseRenderer{
         this.canvas = renderer.canvas;
 
         s.background(0, 0, 0, 255);
+        s.frameRate(15);
+        //s.noLoop();
     }
 
     protected draw(s) {
         if (this.animating) {
 
-            s.background(0, 0, 0, 5);
-         
-            s.beginShape();
-            s.fill(0, 0);
-            s.stroke(250, 213, 20, 255);
-            s.strokeWeight(0.2)
+            s.blendMode(s.BLEND);
+            s.background(0, 0, 0, 15);
 
-            let minRadius = 80;
-            let maxRadius = 100;
+            s.blendMode(s.ADD);
+            s.fill(0, 0);
+            //s.stroke(255, 116, 40, 255); //orange
+            s.stroke(40, 100, 255, 255);   //blue
+            s.strokeWeight(0.2)
 
             s.translate(s.width/2, s.height/2);
 
-            for (let a = 0; a < s.TWO_PI; a += 0.001) {
-                let xoff = s.map(s.cos(a), -1, 1, 0, noiseMax) +
-                                 s.map(s.cos(aoff), -1, 1, 0, noiseMax);
-                let yoff = s.map(s.sin(a), -1, 1, 0, noiseMax) +
-                                 s.map(s.sin(aoff), -1, 1, 0, noiseMax);
-                let r = s.map(s.noise(xoff, yoff), 0, 1, minRadius, maxRadius);
-                let x = r * s.cos(a);
-                let y = r * s.sin(a);
-                s.vertex(x, y);
-            }
-            aoff += 0.01;
+            for (let i = 0; i < 10; i++) {
+                let minRadius = 70;
+                let maxRadius = 120;// + s.sin(aoff) * 10;
 
-            s.endShape(s.CLOSE)
+                s.beginShape();
+                for (let a = 0; a < s.TWO_PI; a += 0.001) {
+                    let xoff = s.map(s.cos(a), -1, 1, 0, noiseMax) +
+                                     s.map(s.cos(aoff), -1, 1, 0, noiseMax);
+    
+                    let yoff = s.map(s.sin(a), -1, 1, 0, noiseMax) +
+                                     s.map(s.sin(aoff), -1, 1, 0, noiseMax);
+    
+                    let r = s.map(s.noise(xoff, yoff), 0, 1, minRadius, maxRadius);
+                    let x = r * s.cos(a);
+                    let y = r * s.sin(a);
+                    s.vertex(x, y);
+                }
+                aoff += 0.01;
+                s.endShape(s.CLOSE)
+            }
 
         }
 
